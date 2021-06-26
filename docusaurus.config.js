40f110de189750a925edf0cdbc4499143d991363
Bob Hall <bhall2001@users.noreply.github.com>
2021-06-26 14:48:09 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,6 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/facebook/docusaurus/edit/master/website/',
-            routeBasePath: '/',
         },
         blog: {
           showReadingTime: true,
